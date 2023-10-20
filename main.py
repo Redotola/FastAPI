6a78bc7dce20e4c6b608212ef5d3903fdd4f7a19
Redotola <redotola117@gmail.com>
2023-10-19 20:24:02 -0600

    Fast API SQLite

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from typing import Optional, List
 from jwt_manager import create_token, validate_token
 from fastapi.security import  HTTPBearer
 from config.database import Session, engine, Base
-from models.movie import Movie
+from models.movie import Movie as MovieModel
 #from starlette.requests import Request
+from fastapi.encoders import jsonable_encoder
 
 Base.metadata.create_all(bind=engine)
 
@@ -41,7 +42,7 @@ class Movie(BaseModel):
                 "overview" : "Descripcion de la pelicula",
                 "year" : 2023,
                 "rating" : 9.8,
-                "category" : "Accion",  
+                "category" : "Accion"
             }
         }
 
@@ -75,6 +76,7 @@ movies = [
 def message():
     return HTMLResponse('<h1>Hello World</h1>')
 
+#method to login user with password and email and create a token for the session
 @app.post('/login', tags = ['auth'])
 def login(user: User):
     if user.email == "user" and user.password == "password":
@@ -85,46 +87,59 @@ def login(user: User):
 # method to obtain all the movies
 @app.get('/movies', tags = ['movies'], response_model=List[Movie], status_code = 200, dependencies = [Depends(JWTBearer())])
 def get_movies() -> List[Movie]:
-    return JSONResponse(status_code = 200,content = movies)
+    db = Session()
+    result = db.query(MovieModel).all()
+    return JSONResponse(status_code = 200,content = jsonable_encoder(result))
 
 #method to obtain movies with an specific id
 @app.get('/movies/{id}', tags = ['movies'], response_model=Movie) #Parameter mode
 def get_movie_by_id(id: int = Path(ge=1, le = 2002)) -> Movie:
-    for item in movies:
-        if item["id"] == id:
-            return JSONResponse(content=item)
-    return JSONResponse(status_code = 404, content=[])
+    db = Session()
+    result = db.query(MovieModel).filter(MovieModel.id == id).first()
+    if not result:
+        return JSONResponse(status_code=404, content = {"message": "No encontrado"})
+    return JSONResponse(status_code = 200, content= jsonable_encoder(result))
 
 
 #method to get movies with a specific category
 @app.get('/movies/', tags = ['movies'], response_model = List[Movie]) #Query mode
 def get_movies_by_category(category: str = Query(min_length = 5, max_length = 15)) -> List[Movie]:
-    data = [item for item in movies if item["category"] == category]
-    return JSONResponse(content=data)
+    db = Session()
+    result = db.query(MovieModel).filter(MovieModel.category == category).all()
+    return JSONResponse(status_code = 200,content = jsonable_encoder(result))
 
 #method to create a new movie
 @app.post('/movies', tags = ['movies'], response_model = dict,status_code = 201)
 def create_movie(movie: Movie) -> dict:
-    movies.append(movie)
+    db = Session()
+    new_movie = MovieModel(**movie.model_dump())
+    db.add(new_movie) # add new movie t
+    db.commit() #update the changes in the database
     return JSONResponse(status_code = 201 ,content={"message": "Se ha registrado la pelicula correctamente"})
 
 #method to update information from a specific movie by id
 @app.put('/movies/{id}', tags = ['movies'], response_model = dict, status_code = 200)
 def update_movie(id : int, movie: Movie) -> dict:
-    for item in movies:
-        if item["id"] == id:
-            item["title"] = movie.title
-            item["overview"] = movie.overview
-            item["year"] = movie.year
-            item["rating"] = movie.rating
-            item["category"] = movie.category
+    db = Session()
+    result = db.query(MovieModel).filter(MovieModel.id == id).first()
+    if not result:
+        return JSONResponse(status_code = 404, content = {"message": "No encontrado"})
+    result.title = movie.title
+    result.category = movie.category
+    result.overview = movie.overview
+    result.rating = movie.rating
+    result.year = movie.year
+    db.commit()
     return JSONResponse(status_code = 200, content = {'mesage': "Se ha actualizado correctamente la informacion de la pelicula"})
 
 #method to delete a movie with id
 @app.delete('/movies/{id}', tags = ['movies'], response_model = dict, status_code = 200)
 def delete_movie(id: int) -> dict:
-    for item in movies:
-        if item["id"] == id:
-            movies.remove(item)
-            return JSONResponse(status_code = 200, content={"mesage": "Se ha eliminado correctamente la pelicula"})
+    db = Session()
+    result = db.query(MovieModel).filter(MovieModel.id == id).first()
+    if not result:
+        return JSONResponse(status_code = 404, content = {"message":"No se encontro"})
+    db.delete(result)
+    db.commit()      
+    return JSONResponse(status_code = 200, content = {"mesage": "Se ha eliminado correctamente la pelicula"})
 
